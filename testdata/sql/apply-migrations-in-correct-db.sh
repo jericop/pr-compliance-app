e9dfff4700b1f65fc60c26f4afe0a59809a56e1e
Jerico Pena <jericop@gmail.com>
2023-10-19 12:45:40 -0400

    Update db/init readme and switch to init shell script
    Use api and migration users

--- testdata/sql/apply-migrations-in-correct-db.sh
+++ testdata/sql/apply-migrations-in-correct-db.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -x
 
-for f in $(ls /docker-entrypoint-initdb.d/*.sql | sort | grep -v 101.sql); do 
-  echo "\$f=$f"
-  psql -U postgres -d pr_compliance < ${f}
+APP_DB="pr_compliance"
+APP_MIG_USER_NAME="pr_compliance_migrations"
+APP_MIG_USER_PASS="pr_compliance_migrations"
+
+export PGPASSWORD="$APP_MIG_USER_PASS" # avoids password prompt
+
+set -x
+
+for f in $(ls /docker-entrypoint-initdb.d/*.sql | sort | grep -v 101.sql); do   
+  psql -v ON_ERROR_STOP=1 --username $APP_MIG_USER_NAME --dbname $APP_DB < ${f}
 done
